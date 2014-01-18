39b03287c22a297d759d61518cdf0eeca42fad4d
Dieter De Paepe <dieter.depaepe@gmail.com>
2014-01-18 13:23:26 +0100

    Update AStarSolver javadoc.

--- src/main/java/com/github/dieterdepaepe/jsearch/search/statespace/solver/AStarSolver.java
+++ src/main/java/com/github/dieterdepaepe/jsearch/search/statespace/solver/AStarSolver.java
@@ -7,18 +7,21 @@ import com.github.dieterdepaepe.jsearch.search.statespace.util.BasicSolution;
 /**
  * An <a href="http://en.wikipedia.org/wiki/A*">A*</a> implementation of a {@link Solver}. It uses a {@link Heuristic}
  * to minimise the number of expanded nodes. It will always find a single {@link Solution}, which is guaranteed
- * to be optimal.
- * <p>
+ * to be optimal (assuming at least one solution state is reachable).
+ * <p/>
  * During search, nodes are expanded on a best-first basis: each time, the node with the lowest total estimated cost
  * will be expanded. Because of this, the speed and memory requirements of this solver are greatly depended on
  * the accuracy of the used {@code Heuristic}.
- * <p>
+ * <p/>
+ * This solver does not use the {@link com.github.dieterdepaepe.jsearch.search.statespace.SearchNode#getSearchSpaceState()}
+ * information.
+ * <p/>
  * This solver assumes an admissible {@code Heuristic}. Should this assumption be violated, and the heuristic
- * overestimates the remaining cost by a factor of {@code e}, the found solution is still guaranteed to be at most
+ * overestimates the remaining cost by a factor of {@code e (> 0)}, the found solution is still guaranteed to be at most
  * <tt>(1 + e)</tt> times more expensive than the actual optimal solution. This technique may be used to speed up
  * the search and decrease memory usage by decreasing the number of visited nodes. Note however that the found
  * solution will still indicate optimality, since the solver assumes an admissible heuristic.
- * <p>
+ * <p/>
  * This implementation is stateless and therefor thread-safe.
  * @author Dieter De Paepe
  */
@@ -31,7 +34,7 @@ public class AStarSolver implements Solver {
                                                 SearchNodeGenerator<T, U> searchNodeGenerator,
                                                 Manager<? super T> manager) {
 
-        FibonacciHeap<InformedSearchNode<T>> heap = new FibonacciHeap<InformedSearchNode<T>>();
+        FibonacciHeap<InformedSearchNode<T>> heap = new FibonacciHeap<>();
         heap.insert(startNode, startNode.getEstimatedTotalCost());
 
         while (!heap.isEmpty() && manager.continueSearch()) {
@@ -44,11 +47,12 @@ public class AStarSolver implements Solver {
 
             T nodeToExpand = informedNodeToExpand.getSearchNode();
             if (nodeToExpand.isGoal()) {
-                manager.registerSolution(new BasicSolution<T>(nodeToExpand, true));
+                manager.registerSolution(new BasicSolution<>(nodeToExpand, true));
                 return;
             }
 
             for (InformedSearchNode<T> successor : searchNodeGenerator.generateSuccessorNodes(nodeToExpand, environment, heuristic)) {
+                // Since A* can be very memory expensive, we do a premature purging of search nodes.
                 if (successor.getEstimatedTotalCost() <= costBound)
                     heap.insert(successor, successor.getEstimatedTotalCost());
             }
