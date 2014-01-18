0794faf404ecbf1bb33abdc3c3b32d573666c216
Dieter De Paepe <dieter.depaepe@gmail.com>
2014-01-18 13:22:13 +0100

    Implement DepthFirstSolver.

---
+++ src/main/java/com/github/dieterdepaepe/jsearch/search/statespace/solver/DepthFirstSolver.java
@@ -0,0 +1,69 @@
+package com.github.dieterdepaepe.jsearch.search.statespace.solver;
+
+import com.github.dieterdepaepe.jsearch.search.statespace.*;
+import com.github.dieterdepaepe.jsearch.search.statespace.util.BasicSolution;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * A {@link com.github.dieterdepaepe.jsearch.search.statespace.Solver} implementation that will examine the state space
+ * in a depth first way. It may find multiple solutions along the way and will return an optimal solution when the state
+ * space has been completely explored.
+ * <p/>
+ * Nodes will be expanded in the order they are provided by the generator. Because of the limited number of nodes
+ * in scope during search, this solver has a very low memory footprint. The depth first expansion makes this solver
+ * unsuited for for infinite depth tree like state spaces or graph-like state space, because the solver could endlessly
+ * wander around the state space without encountering a solution. This problem can be somewhat remedied by defining a
+ * maximum depth in the generator, but this is only possible if a maximum solution depth is known beforehand.
+ * <p/>
+ * This solver will not use the {@link com.github.dieterdepaepe.jsearch.search.statespace.SearchNode#getSearchSpaceState()}
+ * information.
+ * <p/>
+ * This implementation is stateless and therefor thread-safe.
+ * @author Dieter De Paepe
+ */
+public class DepthFirstSolver implements Solver {
+    @Override
+    public <T extends SearchNode, U> void solve(InformedSearchNode<T> startNode,
+                                                U environment,
+                                                Heuristic<? super T, ? super U> heuristic,
+                                                SearchNodeGenerator<T, U> searchNodeGenerator,
+                                                Manager<? super T> manager) {
+        Deque<InformedSearchNode<T>> nodesStack = new ArrayDeque<>();
+
+        T bestGoalNode = null;
+        double bestGoalNodeCost = Double.POSITIVE_INFINITY;
+
+        nodesStack.addFirst(startNode);
+        while (!nodesStack.isEmpty()) {
+            if (!manager.continueSearch())
+                return;
+
+            InformedSearchNode<T> informedNodeToExpand = nodesStack.removeFirst();
+
+            // Don't expand node if it surpasses the cost boundary
+            if (informedNodeToExpand.getEstimatedTotalCost() > manager.getCostBound())
+                continue;
+
+            T searchNode = informedNodeToExpand.getSearchNode();
+            if (searchNode.isGoal()) {
+                if (searchNode.getCost() < bestGoalNodeCost || bestGoalNode == null) {
+                    bestGoalNode = searchNode;
+                    bestGoalNodeCost = searchNode.getCost();
+                }
+                manager.registerSolution(new BasicSolution<>(searchNode, false));
+            } else {
+                // Add generated successor nodes in reverse order to the stack, so they will be evaluated according
+                // to the order defined by the generator.
+                List<InformedSearchNode<T>> successors = searchNodeGenerator.generateSuccessorNodes(searchNode, environment, heuristic);
+                for (int i = successors.size() - 1; i >= 0; i--)
+                    nodesStack.addFirst(successors.get(i));
+            }
+        }
+
+        if (bestGoalNode != null)
+            manager.registerSolution(new BasicSolution<>(bestGoalNode, true));
+    }
+}
---
+++ src/test/java/com/github/dieterdepaepe/jsearch/search/statespace/solver/DepthFirstSolverTest.java
@@ -0,0 +1,95 @@
+package com.github.dieterdepaepe.jsearch.search.statespace.solver;
+
+import com.github.dieterdepaepe.jsearch.problem.dummy.DummyGenerator;
+import com.github.dieterdepaepe.jsearch.problem.dummy.DummyHeuristic;
+import com.github.dieterdepaepe.jsearch.problem.dummy.DummySearchNode;
+import com.github.dieterdepaepe.jsearch.search.statespace.InformedSearchNode;
+import com.github.dieterdepaepe.jsearch.search.statespace.Manager;
+import com.github.dieterdepaepe.jsearch.search.statespace.Solution;
+import com.github.dieterdepaepe.jsearch.search.statespace.dev.LoggingGenerator;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Testing class for {@link com.github.dieterdepaepe.jsearch.search.statespace.solver.DepthFirstSolver}.
+ * @author Dieter De Paepe
+ */
+public class DepthFirstSolverTest {
+    @Test
+    public void testNodeExpansionOrder() {
+        // Search space for this test, nodes are ordered from cheap to expensive, goals nodes are written in capitals.
+        //  ----a----
+        //  |   |   b
+        //  c   |   |
+        // / \  d   |
+        // e |  |   |
+        //   |  F   |
+        //   g      |
+        //   |      H
+        //   I
+
+        DummySearchNode a = new DummySearchNode(0, 0, false);
+        DummySearchNode b = new DummySearchNode(1, 0, false);
+        DummySearchNode c = new DummySearchNode(0.5, 1.5, false);
+        DummySearchNode d = new DummySearchNode(2, 1, false);
+        DummySearchNode e = new DummySearchNode(0, 4, false);
+        DummySearchNode f = new DummySearchNode(5, 0, true);
+        DummySearchNode g = new DummySearchNode(4, 2, false);
+        DummySearchNode h = new DummySearchNode(7, 0, true);
+        DummySearchNode i = new DummySearchNode(8, 0, true);
+
+        ListMultimap<DummySearchNode, DummySearchNode> successors = ArrayListMultimap.create();
+        successors.put(a, c);
+        successors.put(a, d);
+        successors.put(a, b);
+        successors.put(c, e);
+        successors.put(c, g);
+        successors.put(g, i);
+        successors.put(d, f);
+        successors.put(b, h);
+
+        LoggingGenerator<DummySearchNode, Object> generator = new LoggingGenerator<>(new DummyGenerator(successors));
+        DummyHeuristic heuristic = new DummyHeuristic();
+        MyManager manager = new MyManager();
+        DepthFirstSolver solver = new DepthFirstSolver();
+
+        solver.solve(new InformedSearchNode<>(a, 0), null, heuristic, generator, manager);
+
+        assertEquals(generator.getExpandedNodes(), Arrays.asList(a, c, e, g, d, b));
+
+        List<Solution<? extends DummySearchNode>> foundSolutions = manager.solutions;
+        List<DummySearchNode> expectedSolutionNodes =  Arrays.asList(i, f, h, f);
+
+        for (int index = 0; index < foundSolutions.size(); index++) {
+            Solution<? extends DummySearchNode> foundSolution = foundSolutions.get(index);
+            assertEquals(foundSolution.getNode(), expectedSolutionNodes.get(index), "Unexpected search node for index " + index);
+            assertEquals(foundSolution.isOptimal(), index == expectedSolutionNodes.size() - 1, "Incorrect optimality flag for index " + index);
+        }
+    }
+
+    private static class MyManager implements Manager<DummySearchNode> {
+        List<Solution<? extends DummySearchNode>> solutions = new ArrayList<>();
+
+        @Override
+        public boolean continueSearch() {
+            return true;
+        }
+
+        @Override
+        public void registerSolution(Solution<? extends DummySearchNode> solution) {
+            solutions.add(solution);
+        }
+
+        @Override
+        public double getCostBound() {
+            return Double.POSITIVE_INFINITY;
+        }
+    }
+}
